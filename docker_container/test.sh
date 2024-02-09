8b71b236a36ded68f5c214ddbed9a36f0a6a0d0e
Simon Lenau <lenau@cispa.dem>
2024-02-09 16:24:39 +0100

    First try with CI

--- docker_container/test.sh
+++ docker_container/test.sh
@@ -1,5 +1,7 @@
-if [ ${ubuntu_packages} ]; then \
-    for item in ${ubuntu_packages}; do \
-    apt-get install -y ${item}; \
-    done;
-fi;
+export ubuntu_packages=$(cat ./ubuntu_packages)
+
+if [ -n "${ubuntu_packages}" ]; then
+    for item in ${ubuntu_packages}; do
+        echo $item
+    done
+fi
