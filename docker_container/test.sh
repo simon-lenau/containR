b0320376e94bc78299c451858a2ff029feb4d464
Simon Lenau <lenau@cispa.dem>
2024-02-12 12:47:23 +0100

    First try with CI

--- docker_container/test.sh
+++
@@ -1,3 +0,0 @@
-echo "{\"auths\":{\"${CI_REGISTRY}\":{\"oauth2\":\"$(printf "%s" "glpat-mf6XzgTC1o-fPPwx8VH3" | base64 -w 0)\"}}}"
-echo "AAA"
-echo "{\"auths\":{\"blablabla\":{\"auth\":\"$(printf "%s:%s" "xyt" "bbbbbbas" | base64 -w 0)\"}}}"
